99cf70dbddede386a75b3ecb6173137679fbfbd1
Arijit roy <arijit.roy155k@gmail.com>
2024-07-26 08:46:09 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"Use Strict";// treat all js code as newer version
+
+//alert(3 + 3) //We are using nodejs not browser
+
+//console.log(3+3); console.log("Arijit"); // Readibility worst
+
+let name = "Arijit"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to the power 53
+//bigInt 
+//string => '', "";
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => uniqueness
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
