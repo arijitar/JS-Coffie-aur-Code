41cdb27784d91e7be87472e15c55bbc37c29fac3
Arijit roy <arijit.roy155k@gmail.com>
2024-07-26 12:04:06 +0000

    conversion of dataTypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,7 +1,34 @@
+/*let score = true
+let score = undefined
+let score = "Arijit"
+let score = "34acv"*/
 let score = "34"
-
 //console.log(typeof score);
 console.log(typeof (score));
 
-score = Number.parseInt(score)
-console.log(typeof score);
+score = Number.parseInt(score);
+console.log(typeof (score));
+console.log(score);
+let valueInNumber = Number(score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber);
+ 
+//let isLoggedIn = 1
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+/*
+  "33" => 33
+  "34acv" => NaN
+  true => 1, false =>0
+  "" => false
+  1 => true
+  "1" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
