7f7f8cffb6d8d2b35bc6525f9ae436ce56b793e0
Arijit roy <arijit.roy155k@gmail.com>
2024-08-25 20:49:40 +0000

    promises

--- 10_advance_one/promises.js
+++ 10_advance_one/promises.js
@@ -1,107 +1,103 @@
-const promiseOne = new Promise(function(resolve, reject){
-    // Do an async task
-    // DB Calls, cryptography
-    setTimeout(function(){
+const promiseOne = new Promise((resolve, reject) =>{
+    // Do async task like DB call, cryptography, network
+    setTimeout(()=>{
         console.log('Async task is complete');
         resolve()
     },1000)
 })
-promiseOne.then(function(){
+// Consuming Promise
+promiseOne.then(()=>{
     console.log("Promise consumed");
 })
 
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
+new Promise((resolve, reject) => {
+    setTimeout(()=>{
+       console.log("Async task two");
+       resolve()
     },1000)
-}).then(function(){
-    console.log("Async 2 resolved");
+}).then(()=>{
+    console.log("Async two resolved");
     
 })
-
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username: "chai",
-            email: "chaiaurcode@example.com"
-        })
-    }),1000
+// resolve is directly connected inside 'then()'.
+const promiseThree = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+       resolve({ username: "chai", email: "chai@example.com"})
+    },1000)
 })
 
-promiseThree.then(function(user){
-    console.log(user);
+promiseThree.then((user)=>{
+   console.log(user);
 })
 
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if(!error){
-          resolve({
-                username: "arijit",
-                password: "1234"
-            })
-        }else{
-            reject('ERROR: Something went wrong')
-        }
+const promiseFour = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      let error = false
+      if(!error){
+        resolve({username:"Arijit", password: "1234"})
+      }else{
+        reject('ERROR: Something went wrong')
+      }
     },1000)
 })
 
 promiseFour
-.then((user) => {
+.then((user)=>{
     console.log(user);
     return user.username
 })
-.then((username) => {
-    console.log(username);
-    
-}).catch(function(error){
-    console.log(error);
-}).finally(()=>{
-    console.log("The promise is either resolved or rejected");
-    
+.then((username)=>{
+   console.log(username);
+})
+.catch((error)=>{
+  console.log(error);
+})
+.finally(()=>{
+    console.log('The Promise is either resolved or rejected');
 })
 
-const promiseFive = new Promise((resolve, reject) =>{
+
+const promiseFive = new Promise((resolve, reject) => {
     setTimeout(()=>{
-        let error = false
+        let error = true
         if(!error){
-            resolve({username: "javascript", password: "7699"})
+          resolve({username:"Javascript", password: "1234"})
         }else{
-            reject('ERROR: JS Went Wrong')
+          reject('ERROR: Js went wrong')
         }
-    }, 1000)
+      },1000)
 })
 
 async function consumePromiseFive(){
-   try{
-    const response = await promiseFive
+ try{
+    const response =  await promiseFive
     console.log(response);
-    }
-   catch (error){
-     console.log(error);
-   }
+ }
+ catch(error){
+    console.log(error);
+ }
 }
 consumePromiseFive()
 
-// async function getAllUser(){
-//    try{
+// async function getAllUsers(){
+//   try{
 //     const response = await fetch('https://jsonplaceholder.typicode.com/users')
-//     const data =await response.json()
+//     const data = await response.json()
 //     console.log(data);
-    
-//    }catch{
-//     console.log("ERROR: ", error);
-    
-//    }
+//   }catch(error){
+//      console.log("E: ",error);
+     
+//   }
 // }
-// getAllUser()
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response) =>{
+// getAllUsers()
+
+fetch(' https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
     return response.json()
 }).then((data)=>{
     console.log(data);
 })
 .catch((error)=>{
-    console.log(error);
+    console.log(error)
 })
